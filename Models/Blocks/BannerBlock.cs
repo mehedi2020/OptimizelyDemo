87b903b9f1b32842c6a1c14e89396e5f96f1ce52
Mehedi Hasan <mehedi4055@gmail.com>
2025-03-10 19:55:28 +1000

    Added Header and Footer

---
+++ Models/Blocks/BannerBlock.cs
@@ -0,0 +1,31 @@
+﻿using EPiServer.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace OptimizelyDemo.Models.Blocks
+{
+    [SiteContentType(GUID = "11112222-6BC2-4753-902A-8670EDC5F311")]
+    [SiteImageUrl]
+    public class BannerBlock : SiteBlockData
+    {
+
+        [CultureSpecific]
+        [Required(AllowEmptyStrings = false)]
+        [Display(
+        GroupName = SystemTabNames.Content,
+        Order = 1)]
+        public virtual string Title { get; set; }
+
+        [Display(GroupName = SystemTabNames.Content, Order = 2)]
+        [CultureSpecific]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference BackGroundImage { get; set; }
+
+        [Display(
+      GroupName = SystemTabNames.Content,
+      Order = 3
+      )]
+        [CultureSpecific]
+        [UIHint(UIHint.Textarea)]
+        public virtual string Description { get; set; }
+    }
+}
